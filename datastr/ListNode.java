944df18fc633fd67e4f2abdd8ec11b8b7f5a3729
vijayaragavanr <rvijayaragavan86@gmail.com>
2014-03-18 17:15:46 +0530

    Create ListNode.java

---
+++ datastr/ListNode.java
@@ -0,0 +1,43 @@
+package datastr;
+
+public class ListNode {
+/* Author : Vijayaragavan
+ * Date	  : 16-03-2014
+ * */
+	/**
+	 * 1. inserting a node in singly LinkedList at the end
+2. at beginning
+3. at tge middle
+	 */
+
+	private int data;
+	private ListNode next;
+		
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public ListNode getNext() {
+		return this.next;
+	}
+	public void setNext(ListNode next) {
+		this.next = next;
+	}
+	public ListNode(int data) {
+		this.data=data;
+	}
+	
+	public int listLength(ListNode lnode) {
+		int len =0;
+		
+		ListNode currNode = lnode;
+		while(currNode!=null) {
+			len++;
+			currNode = currNode.getNext();
+		}
+				
+		return len;
+	}
+}
